80561c6c3f384c1c82317893076a35e3f5f8bf92
Weverthon Machado <11988227+weverthonmachado@users.noreply.github.com>
2024-07-10 16:35:56 +0200

    fix

--- R/ggpreview.R
+++ R/ggpreview.R
@@ -1,10 +1,10 @@
 #' Preview plot instead of saving. 
 #' 
 #' Useful for checking scale of graphs
-#' @param ... arguments to be passed to [ggplot2::ggsave()]]
+#' @param ... other arguments to be passed to [ggplot2::ggsave()]]
 #' @export
-ggpreview <- function(..) {
-  path <- ggplot2::ggsave(filename = "preview_plot.png", path = tempdir(), ...)
+ggpreview <- function(p, filename, ...) {
+  path <- ggplot2::ggsave(plot = p, filename = "preview_plot.png", path = tempdir(), ...)
   invisible(NULL)
   cli::cli_alert_success("{.file {path}}")
 }
